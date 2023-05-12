9bd52215bf0534fe408b8014038718df08984fdc
Zhang Quan <zhangquan@>
2023-05-12 17:34:31 +0800

    fix debian post-install syntax error

---
+++ files/debian-fix-virtio.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+sed -i '$s/^.*$/&\n\n\n/g'   /target/etc/initramfs-tools/modules
+sed -i '$a# virtio begin'    /target/etc/initramfs-tools/modules
+sed -i '$avirtio_blk'        /target/etc/initramfs-tools/modules
+sed -i '$avirtio_scsi'       /target/etc/initramfs-tools/modules
+sed -i '$avirtio_net'        /target/etc/initramfs-tools/modules
+sed -i '$avirtio_pci'        /target/etc/initramfs-tools/modules
+sed -i '$avirtio_ring'       /target/etc/initramfs-tools/modules
+sed -i '$avirtio'            /target/etc/initramfs-tools/modules
+sed -i '$a# virtio end'      /target/etc/initramfs-tools/modules
+sed -i '$s/^.*$/&\n\n\n/g'   /target/etc/initramfs-tools/modules
+
+in-target update-initramfs -u
+
+
+exit 0
+
