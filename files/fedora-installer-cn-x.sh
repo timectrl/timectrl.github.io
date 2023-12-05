e13dcc26addf624b1db9c10f38866ac823543ed5
Zhang Quan <zhangquan@>
2023-12-05 17:46:20 +0800

    improve fedora installer

--- files/fedora-installer-cn-x.sh
+++ files/fedora-installer-cn-x.sh
@@ -9,16 +9,26 @@ set -x
 
 VER=39
 
-#MIRROR_PREFIX=https://dl.fedoraproject.org/pub/fedora/linux/releases/${VER}
-#MIRROR_PREFIX=https://download-cc-rdu01.fedoraproject.org/pub/fedora/linux/releases/${VER}
-MIRROR_PREFIX=https://mirror.sjtu.edu.cn/fedora/linux/releases/${VER}
-#MIRROR_PREFIX=http://mirrors.163.com/fedora/releases/${VER}
-#MIRROR_PREFIX=http://mirrors.huaweicloud.com/fedora/releases/${VER}
-#MIRROR_PREFIX=http://repo.huaweicloud.com/fedora/releases/${VER}
+MIRROR_PREFIX="https://dl.fedoraproject.org/pub/fedora/linux/releases/${VER}" # 官方
+MIRROR_PREFIX="https://download.fedoraproject.org/pub/fedora/linux/releases/${VER}" # mirror redirect
+MIRROR_PREFIX="http://mirrors.tuna.tsinghua.edu.cn/fedora/releases/${VER}" # 清华大学
+MIRROR_PREFIX="http://mirrors.ustc.edu.cn/fedora/releases/${VER}" # 中国科学技术大学
+MIRROR_PREFIX="http://mirror.sjtu.edu.cn/fedora/linux/releases/${VER}" # 上海交通大学
+MIRROR_PREFIX="http://mirror.nju.edu.cn/fedora/releases/${VER}" # 南京大学
+MIRROR_PREFIX="http://mirror.lzu.edu.cn/fedora/releases/${VER}" # 兰州大学
+MIRROR_PREFIX="http://mirrors.163.com/fedora/releases/${VER}" # 163
+MIRROR_PREFIX="http://mirrors.cloud.tencent.com/fedora/releases/${VER}" # 腾讯云
+MIRROR_PREFIX="http://mirrors.aliyun.com/fedora/releases/${VER}" # 阿里云
+MIRROR_PREFIX="http://mirrors.huaweicloud.com/fedora/releases/${VER}" # 华为云
+MIRROR_PREFIX="http://repo.huaweicloud.com/fedora/releases/${VER}" # 华为云
 
-PREFIX=$(df -h /boot/|grep dev|awk '{print $6}')
-wget -O ${PREFIX}/vmlinuz ${MIRROR_PREFIX}/Everything/x86_64/os/images/pxeboot/vmlinuz
-wget -O ${PREFIX}/initrd.img ${MIRROR_PREFIX}/Everything/x86_64/os/images/pxeboot/initrd.img
+ROOT_PREFIX=$(df -h /boot/|grep dev|head -n 1|awk '{print $6}')
+ROOT_DEVICE=$(df -h /boot/|grep dev|head -n 1|awk '{print $1}')
+mkdir -p ${ROOT_PREFIX}/images
+wget -O ${ROOT_PREFIX}/vmlinuz ${MIRROR_PREFIX}/Everything/x86_64/os/images/pxeboot/vmlinuz
+wget -O ${ROOT_PREFIX}/initrd.img ${MIRROR_PREFIX}/Everything/x86_64/os/images/pxeboot/initrd.img
+wget -O ${ROOT_PREFIX}/images/install.img ${MIRROR_PREFIX}/Everything/x86_64/os/images/install.img
+wget -O ${ROOT_PREFIX}/kickstart.txt http://www.timectrl.net/files/fedora-kickstart-cn-x.txt
 
 EFI=""
 if [ -f /sys/firmware/efi/runtime ]
@@ -31,7 +41,8 @@ cat >>/etc/grub.d/40_custom  <<ENDL
 menuentry "Installer" {
 #set root=(hd0,3)
 set root=(hd0,$(df -h /boot | grep '/dev' | awk '{print $1}'|grep -Eo '[0-9]+'))
-linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/Everything/x86_64/os inst.ks=http://www.timectrl.net/files/fedora-kickstart-cn-x.txt
+#linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/Everything/x86_64/os inst.ks=http://www.timectrl.net/files/fedora-kickstart-cn-x.txt
+linux${EFI} /vmlinuz ip=dhcp inst.repo=hd:${ROOT_DEVICE}:/ inst.ks=hd:${ROOT_DEVICE}:/kickstart-x.txt
 initrd${EFI} /initrd.img
 }
 ENDL
