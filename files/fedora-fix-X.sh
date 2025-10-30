3aec4d3e3b8f5517ef7358065745171ba297dcf6
Zhang Quan <zhangquan@>
2025-10-30 13:53:28 +0800

    add repo of vscode for fedora-X

--- files/fedora-fix-X.sh
+++ files/fedora-fix-X.sh
@@ -36,4 +36,6 @@ sudo dnf -y install android-tools
 sudo dnf -y install --allowerasing ffmpeg mplayer
 
 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
--- files/fedora-fix-dev.sh
+++ files/fedora-fix-dev.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-if [ $UID -ne 0 ]
-then
-	echo "root user only"
-	exit 1
-fi
-
 set -x
 
 
-dnf -y install screen bash-completion
-dnf -y install vim openssl less sqlite tree
-dnf -y install tar gzip bzip2 xz zip unzip unrar p7zip
-dnf -y install wget curl telnet ftp mtr rsync tcpdump iproute
-dnf -y install wireguard-tools openvpn
+sudo dnf -y install screen bash-completion
+sudo dnf -y install vim openssl less sqlite tree
+sudo dnf -y install tar gzip bzip2 xz zip unzip unrar p7zip
+sudo dnf -y install wget curl telnet ftp mtr rsync tcpdump iproute
+sudo dnf -y install wireguard-tools openvpn
 
-dnf -y install make gcc patch diffutils automake libtool pkgconf-pkg-config
-dnf -y install kernel-devel libcurl-devel openssl-devel
-dnf -y install git subversion
-dnf -y install golang
-dnf -y install mariadb
+sudo dnf -y install make gcc patch diffutils automake libtool pkgconf-pkg-config
+sudo dnf -y install kernel-devel libcurl-devel openssl-devel
+sudo dnf -y install git subversion
+sudo dnf -y install golang
+sudo dnf -y install mariadb
 
+
+#sudo dnf -y install code
