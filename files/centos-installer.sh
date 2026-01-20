3bea077490d5347c11d029d6c11e4fbe682620d7
Zhang Quan <zhangquan@>
2026-01-20 13:15:14 +0800

    fix installer rootfs search

--- files/centos-installer.sh
+++ files/centos-installer.sh
@@ -13,7 +13,7 @@ MIRROR_PREFIX="https://mirror.stream.centos.org/${VER}"
 
 ROOT_PREFIX=$(df -h /boot/|grep dev|head -n 1|awk '{print $6}')
 ROOT_DEVICE=$(df -h /boot/|grep dev|head -n 1|awk '{print $1}')
-ROOT_UUID=$(blkid ${ROOT_DEVICE}|sed -e 's/ /\n/g'|grep ^UUID|sed -e 's/^[^"]*"\?//g' -e 's/"\?[^"]*$//g')
+ROOT_UUID=$(blkid ${ROOT_DEVICE}|sed -e 's/ /\n/g'|grep ^UUID|grep -v PARTUUID|sed -e 's/^[^"]*"\?//g' -e 's/"\?[^"]*$//g')
 #wget -O ${ROOT_PREFIX}/vmlinuz ${MIRROR_PREFIX}/BaseOS/x86_64/os/isolinux/vmlinuz
 #wget -O ${ROOT_PREFIX}/initrd.img ${MIRROR_PREFIX}/BaseOS/x86_64/os/isolinux/initrd.img
 wget -O ${ROOT_PREFIX}/vmlinuz ${MIRROR_PREFIX}/BaseOS/x86_64/os/images/pxeboot/vmlinuz
@@ -28,10 +28,10 @@ fi
 sed -i '6,$d' /etc/grub.d/40_custom
 cat >>/etc/grub.d/40_custom  <<ENDL
 menuentry "Installer" {
-
 #set root=(hd0,3)
 #set root=(hd0,$(df -h /boot | grep '/dev' | awk '{print $1}'|grep -Eo '[0-9]+'))
-search --no-floppy --fs-uuid --set ${ROOT_UUID}
+#search --no-floppy --fs-uuid --set ${ROOT_UUID}
+search --no-floppy --fs-uuid --set=root ${ROOT_UUID}
 
 linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/BaseOS/x86_64/os inst.ks=http://www.timectrl.net/files/centos-kickstart.txt inst.text inst.vnc inst.vncpassword=PaFfW0rd
 initrd${EFI} /initrd.img
@@ -40,7 +40,8 @@ initrd${EFI} /initrd.img
 menuentry "Installer-autofs" {
 #set root=(hd0,3)
 #set root=(hd0,$(df -h /boot | grep '/dev' | awk '{print $1}'|grep -Eo '[0-9]+'))
-search --no-floppy --fs-uuid --set ${ROOT_UUID}
+#search --no-floppy --fs-uuid --set ${ROOT_UUID}
+search --no-floppy --fs-uuid --set=root ${ROOT_UUID}
 
 linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/BaseOS/x86_64/os inst.ks=http://www.timectrl.net/files/centos-kickstart-autofs.txt inst.text inst.vnc inst.vncpassword=PaFfW0rd
 initrd${EFI} /initrd.img
--- files/fedora-installer.sh
+++ files/fedora-installer.sh
@@ -24,7 +24,7 @@ MIRROR_PREFIX="http://mirrors.ustc.edu.cn/fedora/releases/${VER}" # 中国科学
 
 ROOT_PREFIX=$(df -h /boot/|grep dev|head -n 1|awk '{print $6}')
 ROOT_DEVICE=$(df -h /boot/|grep dev|head -n 1|awk '{print $1}')
-ROOT_UUID=$(blkid ${ROOT_DEVICE}|sed -e 's/ /\n/g'|grep ^UUID|sed -e 's/^[^"]*"\?//g' -e 's/"\?[^"]*$//g')
+ROOT_UUID=$(blkid ${ROOT_DEVICE}|sed -e 's/ /\n/g'|grep ^UUID|grep -v PARTUUID|sed -e 's/^[^"]*"\?//g' -e 's/"\?[^"]*$//g')
 wget -c -O ${ROOT_PREFIX}/vmlinuz ${MIRROR_PREFIX}/Everything/x86_64/os/images/pxeboot/vmlinuz
 wget -c -O ${ROOT_PREFIX}/initrd.img ${MIRROR_PREFIX}/Everything/x86_64/os/images/pxeboot/initrd.img
 wget -c -O ${ROOT_PREFIX}/fedora-kickstart.txt   http://www.timectrl.net/files/fedora-kickstart.txt
@@ -41,10 +41,10 @@ fi
 sed -i '6,$d' /etc/grub.d/40_custom
 cat >>/etc/grub.d/40_custom  <<ENDL
 menuentry "Installer" {
-
 #set root=(hd0,3)
 #set root=(hd0,$(df -h /boot | grep '/dev' | awk '{print $1}'|grep -Eo '[0-9]+'))
-search --no-floppy --fs-uuid --set ${ROOT_UUID}
+#search --no-floppy --fs-uuid --set ${ROOT_UUID}
+search --no-floppy --fs-uuid --set=root ${ROOT_UUID}
 
 linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/Everything/x86_64/os inst.ks=http://www.timectrl.net/files/fedora-kickstart.txt inst.vnc inst.vncpassword=PaFfW0rd
 #linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/Everything/x86_64/os inst.ks=http://www.timectrl.net/files/fedora-kickstart.txt inst.vnc inst.vncpassword=PaFfW0rd
@@ -57,10 +57,10 @@ initrd${EFI} /initrd.img
 }
 
 menuentry "Installer +X -VNC" {
-
 #set root=(hd0,3)
 #set root=(hd0,$(df -h /boot | grep '/dev' | awk '{print $1}'|grep -Eo '[0-9]+'))
-search --no-floppy --fs-uuid --set ${ROOT_UUID}
+#search --no-floppy --fs-uuid --set ${ROOT_UUID}
+search --no-floppy --fs-uuid --set=root ${ROOT_UUID}
 
 linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/Everything/x86_64/os inst.ks=http://www.timectrl.net/files/fedora-kickstart-x.txt
 #linux${EFI} /vmlinuz ip=dhcp inst.repo=${MIRROR_PREFIX}/Everything/x86_64/os inst.ks=http://www.timectrl.net/files/fedora-kickstart-x.txt
